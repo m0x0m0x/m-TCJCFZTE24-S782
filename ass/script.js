efefe30d79741e90764823c7e89c70e11cbd295b
m0x0m0x <mx0mx@tuta.io>
2024-02-28 19:22:04 +0530

    start

--- ass/script.js
+++ ass/script.js
@@ -1,1 +1,12 @@
-console.log('Hello!');
+"use strict";
+
+const score0El = document.querySelector("#score--0");
+const score1El = document.getElementById("score--1");
+const diceEl = document.querySelector(".dice");
+
+// Starting considitions
+// Reset course
+score0El.textContent = 0;
+score1El.textContent = 0;
+//Hiding the dice
+diceEl.classList.add("hidden");
