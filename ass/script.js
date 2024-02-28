58da1011390fbaa1d86364de217f47b6929fbf17
m0x0m0x <mx0mx@tuta.io>
2024-02-28 23:01:28 +0530

    add

--- ass/script.js
+++ ass/script.js
@@ -1,12 +1,28 @@
 "use strict";
 
+//Scores
 const score0El = document.querySelector("#score--0");
 const score1El = document.getElementById("score--1");
+
+//Dices
 const diceEl = document.querySelector(".dice");
 
+// Selecting buttons
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+
 // Starting considitions
+
 // Reset course
 score0El.textContent = 0;
 score1El.textContent = 0;
 //Hiding the dice
 diceEl.classList.add("hidden");
+
+// Rolling dice functionality
+btnRoll.addEventListener("click", function () {
+  // 1. Generating a random dice roll
+  //2. Display the dice
+  //3. Search for rolled 1 , switch to next player
+});
