3b85a23fbfba75a00f9cf7406f63237f473dbfde
m0x0m0x <mx0mx@tuta.io>
2024-02-29 19:32:22 +0530

    Nice added image

--- ass/script.js
+++ ass/script.js
@@ -20,6 +20,16 @@ score1El.textContent = 0;
 //Hiding the dice
 diceEl.classList.add("hidden");
 
+// Dice Images
+const diceImages = {
+  1: "https://i.postimg.cc/Rhy7HBYk/image.png",
+  2: "https://i.postimg.cc/23NqY8MS/image.png",
+  3: "https://i.postimg.cc/TPvxfNRQ/image.png",
+  4: "https://i.postimg.cc/wBgrwCxK/image.png",
+  5: "https://i.postimg.cc/8kM0VkF8/image.png",
+  6: "https://i.postimg.cc/DZVPCsPS/image.png",
+};
+
 // Rolling dice functionality
 btnRoll.addEventListener("click", function () {
   // 1. Generating a random dice roll
@@ -28,7 +38,7 @@ btnRoll.addEventListener("click", function () {
 
   //2. Display the dice
   diceEl.classList.remove("hidden");
-  diceEl.src = `dice-`;
+  diceEl.src = diceImages[dice];
 
   //3. Search for rolled 1 , switch to next player
 });
